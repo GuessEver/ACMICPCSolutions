ff17e988a58f248bc451cc2aed1669d74571455e
GuessEver <jiangzh777@163.com>
2015-07-26 01:31:52 +0800

    update SPOJ GSS2

--- SPOJ/GSS2/A.cpp
+++ SPOJ/GSS2/A.cpp
@@ -1,35 +1,27 @@
 #include <cstdio>
-#include <cstring>
+#include <map>
 #include <algorithm>
-
+using std::map;
 const int N = 100000 + 10;
-const int inf = 0x3f3f3f3f;
+const long long infll = 0x3f3f3f3f3f3f3f3fll;
 
-int n, m, a[N], pre[N], last[N*2];
+int n, m, a[N];
 struct Query{
-	int idx, l, r;
+	int l, r, i;
 	long long ans;
-	void Read(int i)
-	{
-		scanf("%d%d", &l, &r);
-		idx = i;
-	}
 }q[N];
-long long his[N*4], val[N*4], add[N*4], add2[N*4];
+map<int, int> pos;
+int pre[N];
+long long val[N*4], add_val[N*4], his[N*4], add_his[N*4];
 
-bool cmp_i(Query a, Query b) { return a.idx < b.idx; }
-bool cmp_r(Query a, Query b) { return a.r < b.r; }
-
-void pushDown(int p)
+bool cmp_i(const Query &a, const Query &b)
 {
-	add2[p*2] = std::max(add2[p*2], add2[p] + add[p*2]);
-	add2[p*2+1] = std::max(add2[p*2+1], add2[p] + add[p*2+1]);
-	his[p*2] = std::max(his[p*2], add2[p] + val[p*2]);
-	his[p*2+1] = std::max(his[p*2+1], add2[p] + val[p*2+1]);
-	add2[p] = 0;
-	add[p*2] += add[p];	add[p*2+1] += add[p];
-	val[p*2] += add[p]; val[p*2+1] += add[p];
-	add[p] = 0;
+	return a.i < b.i;
+}
+bool cmp_r(const Query &a, const Query &b)
+{
+	if(a.r == b.r) return a.l < b.l;
+	return a.r < b.r;
 }
 
 void pushUp(int p)
@@ -37,14 +29,26 @@ void pushUp(int p)
 	val[p] = std::max(val[p*2], val[p*2+1]);
 	his[p] = std::max(his[p*2], his[p*2+1]);
 }
+void pushDown(int p)
+{
+	add_his[p*2] = std::max(add_his[p*2], add_val[p*2] + add_his[p]);
+	add_his[p*2+1] = std::max(add_his[p*2+1], add_val[p*2+1] + add_his[p]);
+	his[p*2] = std::max(his[p*2], val[p*2] + add_his[p]);
+	his[p*2+1] = std::max(his[p*2+1], val[p*2+1] + add_his[p]);
+	add_his[p] = 0;
+	//////// Make sure the code above is correct
+	add_val[p*2] += add_val[p]; add_val[p*2+1] += add_val[p];
+	val[p*2] += add_val[p]; val[p*2+1] += add_val[p];
+	add_val[p] = 0;
+}
 
 void modify(int p, int l, int r, int a, int b, int c)
 {
 	if(a <= l && b >= r)
 	{
-		add[p] += c; val[p] += c;
-		add2[p] = std::max(add2[p], add[p]);
+		val[p] += c; add_val[p] += c;
 		his[p] = std::max(his[p], val[p]);
+		add_his[p] = std::max(add_his[p], add_val[p]);
 		return;
 	}
 	int mid = l + (r - l) / 2;
@@ -57,33 +61,22 @@ void modify(int p, int l, int r, int a, int b, int c)
 long long query(int p, int l, int r, int a, int b)
 {
 	if(a <= l && b >= r) return his[p];
-	int mid = l + (r - l) / 2, res1 = 0, res2 = 0;
+	int mid = l + (r - l) / 2; long long res = -infll;
 	pushDown(p);
-	if(a <= mid) res1 = query(p*2, l, mid, a, b);
-	if(b > mid) res2 = query(p*2+1, mid+1, r, a, b);
+	if(a <= mid) res = std::max(res, query(p*2, l, mid, a, b));
+	if(b > mid) res = std::max(res, query(p*2+1, mid+1, r, a, b));
 	pushUp(p);
-	return std::max(res1, res2);
+	return res;
 }
 
 void Solve()
 {
-	std::sort(q+1, q+1+m, cmp_r); int i = 1;
-	for(int t = 1; t <= q[m].r; t++)
+	std::sort(q+1, q+1+m, cmp_r);
+	for(int i = 1, j = 1; i <= n; i++)
 	{
-		if(pre[t]+1 <= t-1) modify(1, 1, n, pre[t]+1, t-1, a[t]);
-		//for(int p = 1; p <= 18; p++) printf("%3lld ", his[p]); puts("");
-		//for(int p = 1; p <= 18; p++) printf("%3lld ", add2[p]); puts("");
-		//for(int p = 1; p <= 18; p++) printf("%3lld ", val[p]); puts("");
-		//for(int p = 1; p <= 18; p++) printf("%3lld ", add[p]); puts(""); puts("");
-		for( ; q[i].r == t; i++)
-		{
-			q[i].ans = query(1, 1, n, q[i].l, q[i].r);
-			//printf("[%d, %d] -- modify:[%d, %d]\n", q[i].l, q[i].r, pre[q[i].r]+1, q[i].r-1);
-			//for(int p = 1; p <= 18; p++) printf("%3lld ", his[p]); puts("");
-			//for(int p = 1; p <= 18; p++) printf("%3lld ", add2[p]); puts("");
-			//for(int p = 1; p <= 18; p++) printf("%3lld ", val[p]); puts("");
-			//for(int p = 1; p <= 18; p++) printf("%3lld ", add[p]); puts(""); puts("");
-		}
+		if(pre[i] + 1 <= i - 1) modify(1, 1, n, pre[i]+1, i-1, a[i]);
+		for(; j <= m && q[j].r <= i; j++)
+			q[j].ans = query(1, 1, n, q[j].l, q[j].r);
 	}
 }
 
@@ -94,15 +87,15 @@ int main()
 	{
 		scanf("%d", &a[i]);
 		modify(1, 1, n, i, i, a[i]);
-		pre[i] = last[a[i]+N];
-		last[a[i]+N] = i;
+		pre[i] = pos[a[i]];
+		pos[a[i]] = i;
 	}
-	//for(int i = 1; i <= n; i++) printf("%d ", pre[i]); puts("");
-	//for(int p = 1; p <= 10; p++) printf("%3lld ", his[p]); puts("");
-	//for(int p = 1; p <= 10; p++) printf("%3lld ", val[p]); puts("");
-	//for(int p = 1; p <= 10; p++) printf("%3lld ", add[p]); puts(""); puts("");
 	scanf("%d", &m);
-	for(int i = 1; i <= m; i++) q[i].Read(i);
+	for(int i = 1; i <= m; i++)
+	{
+		scanf("%d%d", &q[i].l, &q[i].r);
+		q[i].i = i;
+	}
 	Solve();
 	std::sort(q+1, q+1+m, cmp_i);
 	for(int i = 1; i <= m; i++) printf("%lld\n", q[i].ans);
